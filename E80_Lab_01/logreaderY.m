1e5d62b6d11847abda98b96179f36b387d0455cc
nate-noronha <ndabreunoronha@g.hmc.edu>
2026-01-27 23:29:07 -0800

    Update logreaderY.m

--- E80_Lab_01/logreaderY.m
+++ E80_Lab_01/logreaderY.m
@@ -46,50 +46,46 @@ end
 fclose(fid);
 
 %% Process your data here
-% plot(accelX)
-% title('Acceleration in Teensy Units')
-% xlabel('Sample #')
-% ylabel('Teensy Units')
-% hold on;
-% plot(accelY)
-% hold on;
-% plot(accelZ)
-% hold off;
-% legend('Acceleration X-direction','Acceleration Y-direction', 'Acceleration Z-direction');
-% x = accelX
-% y = accelY 
 
-% compute mean, standard deviation, standard error, and 95% confidence
-% bounds for static accel in the z direction
+%do statistics for uncertainty line in Y
+bar = mean(accelY); %mean of all Y values
+S = std(accelY);    %standard deviation of Y values
+N = length(accelY)  %length of accelY 
+df = N-1
+ESE = S/sqrt(N)
+t = 1.999
+lambda = t * ESE
 
 
-f1 = figure;
-f2 = figure;
 
-subplot(3,1,1)
-%put each Z plot, X plot, and Y plot into its own plot in the subplots
-subplot(3,1,1)
-plot(accelX);
-title('Acceleration in X-direction');
-xlabel('Sample #');
-ylabel('Teensy Units');
-legend('Acceleration X-direction')
-hold on;
-subplot(3,1,3)
-plot(accelZ);
-title('Acceleration in Z-direction');
-xlabel('Sample #');
-ylabel('Teensy Units');
-legend('Acceleration Z-direction')
-hold on;
-subplot(3,1,2)
-plot(accelY);
-title('Zero in Y-direction');
-xlabel('Sample #');
-ylabel('Teensy Units');
-legend('Acceleration Y-direction')
-hold off;
 
+
+% Create the uncertainty line for the Y data
+upperBound = bar + lambda
+lowerBound = bar - lambda
+
+
+%plot 3 accel datas
+
+figure
+plot(accelX, '.')
+hold on
+plot(accelY, '.')
+plot(accelZ, '.')
+
+
+
+% create mean and CL lines for zero in Z
+yline(bar, 'b-', 'Mean Y')
+yline(upperBound, 'r--', 'CI upper')
+yline(lowerBound, 'r--', 'CI lower')
+
+
+%label everything
+ylabel('Teensy Units')
+xlabel('Sample Number')
+title('Lab Trial: Zero in Y')
+legend('X data', 'Y data', 'Z data', 'Mean Z', 'CI bounds')
 
 
 
