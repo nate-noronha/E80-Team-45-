6f1384879aaff05ef9b5b6bcb67d3a23c56d18db
William Ortega <wortega@g.hmc.edu>
2026-04-01 19:39:20 -0700

    Update DepthControl.h

--- libraries/main/DepthControl.h
+++ libraries/main/DepthControl.h
@@ -35,7 +35,7 @@ public:
   float depth_des;   // desired depth
   float depth;       // current depth
   float depth_error; // distance to waypoint
-  float Kp=00.0;     // proportional control gain
+  float Kp=100.0;     // proportional control gain
   float uV;          // vertical motor effort
 
   bool diveState = 1;
